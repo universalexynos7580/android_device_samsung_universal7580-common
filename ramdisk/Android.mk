50115c02f8ab7daf00316f3a4c23956d5746492f
Flominator <github.flominatorgd@gmail.com>
2022-12-04 16:46:34 +0100

    universal7580-common: move ueventd.samsungexynos7580.rc to vendor/etc

--- ramdisk/Android.mk
+++ ramdisk/Android.mk
@@ -68,9 +68,10 @@ include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE		:= ueventd.samsungexynos7580.rc
+LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS	:= optional
 LOCAL_MODULE_CLASS	:= ETC
 LOCAL_SRC_FILES		:= etc/ueventd.samsungexynos7580.rc
-LOCAL_MODULE_PATH	:= $(TARGET_ROOT_OUT)
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/etc
 include $(BUILD_PREBUILT)
 
