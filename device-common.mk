3ac1e39687634b79cd2c1d38a4b0481f5bb3099b
flominatorgd <github.flominatorgd@gmail.com>
2023-05-24 01:12:03 +0200

    universal7580-common: remove vendorsetup.sh and import legacy camera device provider and motor as a custome solution for universal7580

--- device-common.mk
+++ device-common.mk
@@ -54,7 +54,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    android.hardware.camera.provider@2.4-impl-legacy \
+    android.hardware.camera.provider@2.4-impl-legacy.universal7580 \
     android.hardware.camera.provider@2.4-service
 
 # Configstore
